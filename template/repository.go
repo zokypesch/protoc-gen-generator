06bddad0869f4631f88beb97496aa1a089552a5b
zokypesch <maulanaoktofitriadi@gmail.com>
2020-03-31 22:04:42 +0700

    remove unused

--- template/repository.go
+++ template/repository.go
@@ -57,8 +57,6 @@ func (repo *{{ ucfirst $msg.Name }}Repository) GetAll(payload *{{ ucfirst $msg.N
 	var data []{{ ucfirst $msg.Name }}
 	var total int
 
-	{{ unescape "{" }}{{ unescape "}" }}
-
 	db := repo.db.Set("gorm:auto_preload", true).Mode(&{{ ucfirst $msg.Name }}{{ unescape "{" }}{{ unescape "}" }}).Where(payload)
 	db.Count(&total)
 	db.Offset(offset).Limit(limitInit).Find(&data)
